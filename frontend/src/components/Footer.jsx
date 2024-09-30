a9584cb31dc2b78db37cf8b6b7b24694193c41f5
kdev307 <127479436+kdev307@users.noreply.github.com>
2024-09-30 20:19:36 +0530

    Updated Footer Templating

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,30 @@ function Footer() {
     return (
         <>
             <footer className="footer">
-                <a href="#" className="logo">
-                    <h2>space resrved for Logo</h2>
-                </a>
-                <div className="other-info">Other Information</div>
+                <div className="main-info">
+                    <a href="#" className="logo">
+                        <h2>space resrved for Logo</h2>
+                    </a>
+                    <div className="social-media">
+                        <ul className="social-media-list">
+                            <li className="social-media-platform">Meta</li>
+                            <li className="social-media-platform">Instagram</li>
+                            <li className="social-media-platform">LinkedIn</li>
+                            <li className="social-media-platform">X</li>
+                            <li className="social-media-platform">Youtube</li>
+                        </ul>
+                    </div>
+                    <div className="contact">
+                        <ul className="contact-list">
+                            <li className="contact-list-item">Mobile No.</li>
+                            <li className="contact-list-item">Mail</li>
+                            <li className="contact-list-item">Address</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="other-info">
+                    <h3>Other informations</h3>
+                </div>
             </footer>
         </>
     );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 function Home() {
     return (
         <div className="home-page">
             <Navbar />
             <Hero />
+            <Footer />
         </div>
     );
 }
