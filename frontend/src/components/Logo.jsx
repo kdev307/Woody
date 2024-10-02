bfa9393ac7899776c37c665d1cb872de3070c894
kdev307 <127479436+kdev307@users.noreply.github.com>
2024-10-02 20:31:22 +0530

    Created Navbar

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/logo.css";
+
+function Logo() {
+    return (
+        <div className="logo">
+            <div className="tagLine top">Inspired by Nature</div>
+            <div className="brand-name">Woody</div>
+            <div className="tagLine bottom">Designed for Life</div>
+        </div>
+    );
+}
+
+export default Logo;
