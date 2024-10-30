bbf630f9f075a259c384d8d3a65dd7d4fabd6aa6
kdev307 <127479436+kdev307@users.noreply.github.com>
2024-10-30 15:49:20 +0530

    Some more CSS Enhancements on Updates

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,65 +1,4 @@
-// import { Edit } from "@mui/icons-material";
-// import React from "react";
-
-// function Profile() {
-//     return (
-//         <div className="profile-section">
-//             <h1 className="profile-title">My Profile</h1>
-//             <Close
-//                 className="cart-close-btn"
-//                 style={{ fontSize: "2.4rem" }}
-//                 onClick={() => {
-//                     handleCartToggle();
-//                 }}
-//             />
-//             {/* <Edit className="edit-btn" /> */}
-//             <div className="main-info">
-//                 <img src="" alt="" className="profile-img" />
-//                 <div className="basic-info">
-//                     <h2 className="name">Ethan Mathew Hunt</h2>
-//                     <h3 className="mobile">787-456-3698</h3>
-//                     <h3 className="mail">ethan.hunt@imf.us</h3>
-//                 </div>
-//                 <p className="endLine">
-//                     <hr
-//                         style={{
-//                             width: "200%",
-//                             margin: "auto",
-//                             border: "0.1rem solid #014210",
-//                             borderRadius: "100rem",
-//                         }}
-//                     />
-//                 </p>
-//                 <div className="additional-info">
-//                     <h4 className="username">Bravo Echo 11</h4>
-//                     <h4 className="dob">August 18, 1964</h4>
-//                     <h4 className="gender">Male</h4>
-//                     <h4 className="address">
-//                         65C, Downtown, Manhattan, New York, United States of America - 10013
-//                     </h4>
-//                 </div>
-//                 <p className="endLine">
-//                     <hr
-//                         style={{
-//                             width: "200%",
-//                             margin: "auto",
-//                             border: "0.1rem solid #014210",
-//                             borderRadius: "100rem",
-//                         }}
-//                     />
-//                 </p>
-//                 <div className="profile-btns">
-//                     <button className="profile-btn cancel-btn">Edit Profile</button>
-//                     <button className="profile-btn edit-btn">Order History</button>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Profile;
-
-import { Edit, Close, History } from "@mui/icons-material";
+import { Edit, Close, History, Person } from "@mui/icons-material";
 import React from "react";
 import "../styles/common.css";
 import "../styles/profile.css";
@@ -72,7 +11,10 @@ function Profile({ handleProfileToggle, className }) {
                 style={{ fontSize: "2.4rem" }}
                 onClick={handleProfileToggle}
             />
-            <h1 className="profile-title">My Profile</h1>
+            <div className="profile-heading">
+                <h1 className="profile-title">My Profile</h1>
+                <Person style={{ fontSize: "2.4rem", color: "#014210" }} />
+            </div>
 
             <div className="basic-info">
                 <img src="" alt="Ethan Hunt" className="profile-img" />
@@ -102,7 +44,16 @@ function Profile({ handleProfileToggle, className }) {
                     Address : 65C, Downtown, Manhattan, New York, United States of America - 10013
                 </h4>
             </div>
-
+            <p className="endLine">
+                <hr
+                    style={{
+                        width: "100%",
+                        margin: "auto",
+                        border: "0.1rem solid #014210",
+                        borderRadius: "100rem",
+                    }}
+                />
+            </p>
             <div className="profile-btns">
                 <button className="profile-btn edit-btn">
                     Edit Profile <Edit />
