73d1dd03fac376a3dc8d8ad0f853c64deb5ae7fb
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-03-15 16:36:37 +0530

    Added Lazy Loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,26 @@
-// import react from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./ProtectedRoute";
-// import { AuthProvider, useAuth } from "./context/AuthContext";
-
-import Home from "./pages/Home";
-import Store from "./pages/Store";
-import NotFound from "./pages/NotFound";
-import Product from "./pages/Product";
-import Confirmed from "./pages/Confirmed";
-import Checkout from "./pages/Checkout";
-import OrderHistory from "./pages/OrderHistory";
-import OrderDispatch from "./pages/OrderDispatch";
-import UserReviews from "./pages/UserReviews";
+import React, { useEffect, Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { USER_LOGIN_SUCCESS } from "./constants/userConstants";
-// import Profile from "./components/Profile";
-// import Categories from "./components/Categories";
+import ProtectedRoute from "./ProtectedRoute";
+import Loader from "./components/Loader";
+
+// Lazy load components
+const Home = lazy(() => import("./pages/Home"));
+const Store = lazy(() => import("./pages/Store"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Product = lazy(() => import("./pages/Product"));
+const Confirmed = lazy(() => import("./pages/Confirmed"));
+const Checkout = lazy(() => import("./pages/Checkout"));
+const OrderHistory = lazy(() => import("./pages/OrderHistory"));
+const OrderDispatch = lazy(() => import("./pages/OrderDispatch"));
+const UserReviews = lazy(() => import("./pages/UserReviews"));
 
 function App() {
     const cartItemsList = useSelector((state) => state.cart.cartItemsList);
     const { userInfo } = useSelector((state) => state.userLogin);
-    // const { order } = useSelector((state) => state.orderCreate);
 
     const dispatch = useDispatch();
-
-    // useEffect(() => {
-    //     const storedUserInfo = localStorage.getItem("userInfo");
-    //     if (storedUserInfo) {
-    //         // If there's a logged-in user in localStorage, dispatch the action to store that in Redux
-    //         dispatch({
-    //             type: USER_LOGIN_SUCCESS,
-    //             payload: JSON.parse(storedUserInfo),
-    //         });
-    //     }
-    // }, [dispatch]);
 
     useEffect(() => {
         const userInfoFromStorage = localStorage.getItem("userInfo")
@@ -49,71 +35,69 @@ function App() {
         }
     }, [dispatch]);
 
-    console.log("User Info: ", userInfo);
     const isAdmin = userInfo ? userInfo.isAdmin : false;
 
     return (
         <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/store" element={<Store />} />
-                    <Route path="/product/:id" element={<Product />} />
+            <Router>
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/store" element={<Store />} />
+                        <Route path="/product/:id" element={<Product />} />
 
-                    {/* Protected Routes */}
-                    <Route
-                        path="/checkout"
-                        element={
-                            <ProtectedRoute
-                                redirectTo="/store"
-                                condition={() => cartItemsList.length > 0}
-                            >
-                                <Checkout />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/confirmed"
-                        element={
-                            <ProtectedRoute
-                                redirectTo="/store"
-                                // condition={() => order}
-                            >
-                                <Confirmed />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/order-history"
-                        element={
-                            <ProtectedRoute redirectTo="/store">
-                                <OrderHistory />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/reviews"
-                        element={
-                            <ProtectedRoute redirectTo="/store">
-                                <UserReviews />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/admin/order-dispatch"
-                        element={
-                            <ProtectedRoute
-                                requiredRole={isAdmin ? "admin" : ""}
-                                redirectTo="/"
-                            >
-                                <OrderDispatch />
-                            </ProtectedRoute>
-                        }
-                    />
+                        {/* Protected Routes */}
+                        <Route
+                            path="/checkout"
+                            element={
+                                <ProtectedRoute
+                                    redirectTo="/store"
+                                    condition={() => cartItemsList.length > 0}
+                                >
+                                    <Checkout />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/confirmed"
+                            element={
+                                <ProtectedRoute redirectTo="/store">
+                                    <Confirmed />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/order-history"
+                            element={
+                                <ProtectedRoute redirectTo="/store">
+                                    <OrderHistory />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/reviews"
+                            element={
+                                <ProtectedRoute redirectTo="/store">
+                                    <UserReviews />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/admin/order-dispatch"
+                            element={
+                                <ProtectedRoute
+                                    requiredRole={isAdmin ? "admin" : ""}
+                                    redirectTo="/"
+                                >
+                                    <OrderDispatch />
+                                </ProtectedRoute>
+                            }
+                        />
 
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Suspense>
+            </Router>
         </div>
     );
 }
--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function Loader() {
     return (
-        <div className="loading-container p-auto flex items-center justify-center">
+        <div className="loading-container p-auto flex flex-col items-center justify-center">
             <div className="loader m-8 border-10 border-[#c2daba] border-t-8 border-t-[#014210] rounded-full w-40 h-40 animate-spin"></div>
+            <h2 className="text-3xl font-semibold">
+                Hold On, Loading Resources
+            </h2>
         </div>
     );
 }
